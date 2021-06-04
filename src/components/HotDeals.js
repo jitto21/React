99b054c04d26dd38af816406dfce04480b3494c6
Jitto Thomas <jittotp@gmail.com>
2021-06-04 16:14:34 +0530

    Hot Deals Initial Draft

--- src/components/HotDeals.js
+++ src/components/HotDeals.js
@@ -3,11 +3,11 @@ import React from 'react'
 export default class HotDeals extends React.Component {
     render() {
         return <div class="card" style={{width: 240}}>
-            <img class="card-img-top" src="public/logo192.png" alt="Card cap"/>
+            <img class="card-img-top" src={'/logo192.png'} alt="Card cap"/>
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">React App</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="uhu" className="btn btn-primary">Go somewhere</a>
+                <a href="uhu" className="btn btn-primary">Go to Docs</a>
             </div>
         </div>
     }
